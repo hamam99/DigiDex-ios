f074b97669a4ee2d3963fcce9840492109125c69
Hamam <zero.saturnus@gmail.com>
2026-08-29 00:04:42 +0800

    clean up

--- DigiDex/ContentView.swift
+++ DigiDex/ContentView.swift
@@ -9,8 +9,6 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    // @Environment(\.modelContext) private var modelContext
-    // @Query private var favouriteDigimon: [DigimonFavouriteModel]
 
     var body: some View {
         ZStack {
